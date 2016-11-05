4a85b767d3f303ddf57d306a2c3e0978bd0c8be1
twilson33 <twilson9182@gmail.com>
2016-11-04 22:32:28 -0500

    made rocket js page

---
+++ js/rocket.js
@@ -0,0 +1,31 @@
+var timer = null;
+var countDownNumber = 10;
+var changeState = function(state){
+	document.body.className = 'body-state'+state;
+	clearInterval(timer);
+	countDownNumber = 10;
+document.getElementById('countdown').innerHTML = countDownNumber;
+	if(state == 2){
+		timer = setInterval(function(){
+		countDownNumber = countDownNumber - 1;
+		document.getElementById('countdown').innerHTML = countDownNumber;
+		if(countDownNumber == 0){
+			changeState(3);
+		}
+		}, 500)
+	}
+	else if(state == 3){
+		var success = setTimeout(function(){
+var randomNumber = Math.round(Math.random()*10);
+		console.log("random number: " + randomNumber);
+		//success
+		if(randomNumber > 5){
+			changeState(4);
+		}
+		else{
+			changeState(5);
+		}
+		}, 2000)
+	}
+
+}
